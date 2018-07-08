93be0a2381a9ee99b73a92fa7b5f384e45ee5569
Huaso <cristian.espinoza.s@usach.cl>
2018-07-08 01:03:37 -0400

    SUbiendo primer commit

---
+++ src/main/java/MermeLab/MermeLab_V1/MermeLabV1Application.java
@@ -0,0 +1,12 @@
+package MermeLab.MermeLab_V1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MermeLabV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MermeLabV1Application.class, args);
+	}
+}
